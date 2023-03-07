517459b9bbc10612dc0fa20e604be2a521dac393
MrHarshalP <Patilh148@gmail.com>
2023-03-07 19:49:29 +0530

    initial commit

---
+++ file1.java
@@ -0,0 +1,19 @@
+
+/* 1. Write a Program to store any ten numbers from the user and display them */ 
+
+package one_Dimensional_Array;
+
+public class Store10_nu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		     
+			    int i;
+				System.out.println ("The first 10 natural numbers are:\n");
+				for (i=1;i<=10;i++)
+				{      
+					System.out.println (i);
+				}
+			System.out.println ("\n");
+			}
+}
